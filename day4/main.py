fe1083929c741f8cbf0ad0fda98af292af606238
Bruno Carvalhal <bruno@email.com>
2021-12-04 11:25:28 +0100

    Implement day 4

---
+++ day4/main.py
@@ -0,0 +1,55 @@
+lines = open("day4/input").read().strip().split("\n\n")
+
+# Parse input numbers
+nums = lines[0].split(",")
+
+# Parse bingo cards
+class Card:
+    def __init__(self, rows, cols):
+        self.rows, self.cols = rows, cols
+
+def parseCard(card):
+    rows = [row.split() for row in card.split("\n")]
+    cols = [[row[i] for row in rows] for i in range(len(rows[0]))]
+    return Card(rows, cols)
+
+cards = [parseCard(card) for card in lines[1:]]
+
+# mark and try to finish line
+def tryFinish(line, num):
+    finished = False
+    if num in line:
+        line.remove(num)
+        finished = len(line) == 0
+    return finished
+
+# mark a number on a card
+def nextMove(card, num):
+    finished = False
+    for row in card.rows:
+        finished |= tryFinish(row, num)
+    for col in card.cols:
+        finished |= tryFinish(col, num)
+    return finished
+
+# calculate result for a winning card
+def calculateResult(card, num):
+    return sum([sum(map(int, row)) for row in card.rows]) * int(num)
+
+def part1(nums, cards):
+    for num in nums:
+        for card in cards:
+            if nextMove(card, num):
+                return calculateResult(card, num)
+
+def part2(nums, cards):
+    winners = set()
+    for num in nums:
+        for card in [c for c in cards if c not in winners]:
+            if nextMove(card, num):
+                winners.add(card)
+                if len(cards) == len(winners):
+                    return calculateResult(card, num)
+
+print(part1(nums, cards))
+print(part2(nums, cards))
