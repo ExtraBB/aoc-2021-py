b700ce0af5820f8b672dcb223912174853f08ae9
Bruno Carvalhal <bruno.carvalhal@datasnipper.com>
2021-12-06 10:16:28 +0100

    Shorter implementation

--- day6/main.py
+++ day6/main.py
@@ -4,17 +4,11 @@ nums = [int(i) for i in open("day6/input").read().strip().split(",")]
 
 def simulate_fish(days):
     counter = Counter(nums)
-    for i in range(days):
+    for _ in range(days):
         copy = counter.copy()
-        copy[8] = counter[0]
-        copy[7] = counter[8]
-        copy[6] = counter[0] + counter[7]
-        copy[5] = counter[6]
-        copy[4] = counter[5]
-        copy[3] = counter[4]
-        copy[2] = counter[3]
-        copy[1] = counter[2]
-        copy[0] = counter[1]
+        for i in range(9):
+            copy[i] = counter[(i + 1) % 9]
+        copy[6] += counter[0]
         counter = copy
     return sum(counter.values())
 
