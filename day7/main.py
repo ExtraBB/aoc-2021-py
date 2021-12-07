1991b5b178e554ba6e94c229ebd7a05c86ce55da
Bruno Carvalhal <bruno.carvalhal@datasnipper.com>
2021-12-07 09:04:45 +0100

    Use binary search

--- day7/main.py
+++ day7/main.py
@@ -6,6 +6,17 @@ def calculate_fuel1(nums, goal):
 def calculate_fuel2(nums, goal):
     return sum([sum(range(abs(i - goal) + 1)) for i in nums])
 
-print(min([calculate_fuel1(nums, i) for i in range(min(nums), max(nums) + 1)]))
-print(min([calculate_fuel2(nums, i) for i in range(min(nums), max(nums) + 1)]))
+def binary_search(goals, minIndex, maxIndex, func):
+    if maxIndex - minIndex < 2:
+        return min(func(nums, goals[minIndex]), func(nums, goals[maxIndex]))
+    else:
+        mid = minIndex + (maxIndex - minIndex) // 2
+        goal1 = func(nums, goals[mid])
+        goal2 = func(nums, goals[mid - 1])
+        return binary_search(goals, minIndex, mid, func) if goal1 > goal2 else binary_search(goals, mid, maxIndex, func)
 
+start = min(nums)
+end = max(nums) + 1
+goals = range(start, max(nums) + 1)
+print(binary_search(goals, start, end, calculate_fuel1))
+print(binary_search(goals, start, end, calculate_fuel2))
