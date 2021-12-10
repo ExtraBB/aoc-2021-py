c34aaf2b9280d3ec8280a6412a4a3c5d0ee4c05f
Bruno Carvalhal <bruno@email.com>
2021-12-10 16:44:39 +0100

    cleanup

--- day9/main.py
+++ day9/main.py
@@ -6,48 +6,29 @@ all_directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
 def valid_coordinates(x, y):
     return y >= 0 and y < len(lines) and x >= 0 and x < len(lines[y])
 
-def valid_neighbours(x, y, directions):
-    return [(x + dx, y + dy) for (dx, dy) in directions if valid_coordinates(x + dx, y + dy)]
+def valid_neighbours(x, y):
+    return [(x + dx, y + dy) for (dx, dy) in all_directions if valid_coordinates(x + dx, y + dy)]
 
-def is_low_point(x, y, directions):
-    return all([lines[y2][x2] > lines[y][x] for (x2, y2) in valid_neighbours(x, y, directions)])
+def is_low_point(x, y):
+    return all([lines[y2][x2] > lines[y][x] for (x2, y2) in valid_neighbours(x, y)])
 
 def find_low_points():
     points = []
     for y in range(len(lines)):
         for x in range(len(lines[y])):
-            if is_low_point(x, y, all_directions):
+            if is_low_point(x, y):
                 points.append((x, y))
     return points
 
-def filter_directions(x, y, basin):
-    filtered = []
-
-    for (dx, dy) in all_directions:
-        if not valid_coordinates(x + dx, y + dy):
-            continue
-        if (x + dx, y + dy) in basin:
-            continue
-        if lines[y + dy][x + dx] == 9:
-            continue
-        filtered.append((dx, dy))
-    
-    return filtered
-
 def find_basins():
-    basins = []
-    for (x, y) in find_low_points():
-        basin = set()
-        extend_basin(basin, x, y)
-        basins.append(basin)
-    return basins
+    return [follow_basin(set(), x, y) for (x,y) in find_low_points()]
 
-def extend_basin(basin, x, y):  
+def follow_basin(basin, x, y):  
     basin.add((x, y))
-    directions = filter_directions(x, y, basin)
-    for (dx, dy) in directions:
-        extend_basin(basin, x + dx, y + dy)
-
+    for (x2, y2) in valid_neighbours(x, y):
+        if lines[y2][x2] != 9 and (x2, y2) not in basin:
+            follow_basin(basin, x2, y2)
+    return basin
 
 print(sum([lines[y][x] + 1 for (x,y) in find_low_points()]))
 print(prod(sorted([len(basin) for basin in find_basins()], reverse=True)[:3]))
